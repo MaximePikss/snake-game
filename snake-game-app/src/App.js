d4ef24c2b003657652d33489b22ac601094e798a
magb7 <margaux.bedu@gmail.com>
2020-04-08 16:05:53 +0200

    add methods for the game

--- snake-game-app/src/App.js
+++ snake-game-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SnakeDot from "./components/SnakeDot";
 import Food from "./components/Food";
 
+// méthode qui permet de donner des coordonnées aléatoires à food
 const getRandomCoordinates = () => {
   let min = 1;
   let max = 98;
@@ -10,14 +11,143 @@ const getRandomCoordinates = () => {
   return [x, y];
 };
 
+const initialeState = {
+  direction: "RIGHT",
+  speed: 200,
+  food: getRandomCoordinates(),
+  snakeDots: [
+    [0, 0],
+    [2, 0],
+  ],
+};
+
 class App extends Component {
-  state = {
-    food: getRandomCoordinates(),
-    snakeDots: [
-      [0, 0],
-      [2, 0],
-    ],
+  // State
+  state = initialeState;
+
+  // Méthode pour le mouvement du snake
+  componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed);
+    document.onkeydown = this.onKeyDown;
+  }
+  //Méthode pour vérifier si le snake a été mis à jour
+  componentDidUpdate() {
+    this.checkIfOutOfBorders();
+    this.checkIfCollapsed();
+    this.checkIfEat();
+  }
+
+  // Méthode pour donner des directions en fonction de la touche appuyée
+  onKeyDown = (e) => {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 38:
+        this.setState({ direction: "UP" });
+        break;
+      case 40:
+        this.setState({ direction: "DOWN" });
+        break;
+      case 37:
+        this.setState({ direction: "LEFT" });
+        break;
+      case 39:
+        this.setState({ direction: "RIGHT" });
+        break;
+      default:
+        return;
+    }
   };
+
+  //Méthode pour faire bouger le snake
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots]; // récupère les coordonnées sur snake
+    let head = dots[dots.length - 1]; // récupère la tête du snake qui sont les dernières coordonnées du snake
+
+    // check la direction avec un switch
+
+    switch (this.state.direction) {
+      case "RIGHT":
+        head = [head[0] + 2, head[1]];
+        break;
+      case "LEFT":
+        head = [head[0] - 2, head[1]];
+        break;
+      case "UP":
+        head = [head[0], head[1] - 2];
+        break;
+      case "DOWN":
+        head = [head[0], head[1] + 2];
+        break;
+      default:
+        return "Invalid Direction";
+    }
+    //ajoute la nouvelle tête au snake
+    dots.push(head);
+    //enlever les premières coordonnées = queue du snake
+    dots.shift();
+    //change le state
+    this.setState({
+      snakeDots: dots,
+    });
+  };
+
+  //Méthode pour contraindre le snake dans la game-area
+  checkIfOutOfBorders() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      this.onGameOver();
+    }
+  }
+
+  //Méthode pour savoir si le snake s'est mangé lui-même
+  checkIfCollapsed() {
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop(); //retire la tête du snake
+    snake.forEach((dot) => {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
+        this.onGameOver();
+      }
+    });
+  }
+
+  //Méthode le snake mange la nourriture
+  checkIfEat() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    let food = this.state.food;
+    if (head[0] === food[0] && head[1] === food[1]) {
+      //on met à jour la position de la nourriture avant sinon boucle infinie !
+      this.setState({
+        food: getRandomCoordinates(),
+      });
+      this.enlargeSnake();
+      this.increaseSpeed();
+    }
+  }
+
+  //Méthode pour faire grandir le snake lorsqu'il mange
+  enlargeSnake() {
+    let newSnake = [...this.state.snakeDots];
+    newSnake.unshift([]);
+    this.setState({
+      snakeDots: newSnake,
+    });
+  }
+
+  //Méthode pour augmenter la vitesse du snake
+  increaseSpeed() {
+    if (this.state.speed > 10) {
+      this.setState({
+        speed: this.state.speed - 10,
+      });
+    }
+  }
+
+  //Méthode game over
+  onGameOver() {
+    alert(`Game Over ! Snake length is ${this.state.snakeDots.length}`);
+    this.setState(initialeState);
+  }
 
   render() {
     return (
