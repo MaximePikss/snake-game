88e4b3710345ea3d8ede2432ca4865693daca243
magb7 <margaux.bedu@gmail.com>
2020-04-08 12:07:07 +0200

    add food in App

--- snake-game-app/src/App.js
+++ snake-game-app/src/App.js
@@ -1,23 +1,24 @@
-import React, {Component} from 'react';
-import SnakeDot from './components/SnakeDot';
-
+import React, { Component } from "react";
+import SnakeDot from "./components/SnakeDot";
+import Food from "./components/Food";
 
 class App extends Component {
   state = {
+    food: [6, 8],
     snakeDots: [
       [0, 0],
       [2, 0],
-    ]
-  }
+    ],
+  };
 
   render() {
     return (
       <div className="game-area">
-       <SnakeDot snakeDots={this.state.snakeDots}/> 
+        <SnakeDot snakeDot={this.state.snakeDots} />
+        <Food foodDot={this.state.food} />
       </div>
     );
   }
-  
 }
 
 export default App;
