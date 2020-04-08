16b9540d03cabc088e0c89537255cde1cf09faa8
magb7 <margaux.bedu@gmail.com>
2020-04-08 11:45:28 +0200

    creat component file

---
+++ snake-game-app/src/components/SnakeDot.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SnakeDot = (props) => {
+ return (
+  <div>
+   {props.snakeDots.map((dot, i) => {
+    const coord = {
+     left: `${dot[0]}%`,
+     top: `${dot[1]}%`,
+    };
+    return <div className="snake-dot" key={i} style={coord}></div>;
+   })}
+  </div>
+ );
+};
+
+export default SnakeDot;
